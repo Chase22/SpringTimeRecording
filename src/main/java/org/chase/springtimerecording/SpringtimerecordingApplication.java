7ba3be7af620f2139b9c01c50093d65e013c480b
chase <chase@mailbox.org>
2018-09-17 20:48:11 +0200

    Initialize Repository

---
+++ src/main/java/org/chase/springtimerecording/SpringtimerecordingApplication.java
@@ -0,0 +1,12 @@
+package org.chase.springtimerecording;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringtimerecordingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringtimerecordingApplication.class, args);
+    }
+}
---
+++ src/test/java/org/chase/springtimerecording/SpringtimerecordingApplicationTests.java
@@ -0,0 +1,16 @@
+package org.chase.springtimerecording;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringtimerecordingApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
